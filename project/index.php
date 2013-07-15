de7ee2827276e7e9057d914eefda8aeeb720f0b8
Kim Cooperrider <kim.cooperrider@gmail.com>
2013-07-15 17:12:48 -0500

    some restructuring of gameboard

--- project/index.php
+++ project/index.php
@@ -6,21 +6,26 @@
   <br>
   <br>
 
-  <div class="row" id="hangman" style="display:none">
-    <div class="well span3 hangman-image">
+  <div class="row" id="hangman" style="display:none1">
+    <div class="well span7 hangman-area">
       <img src="http://placebear.com/200/300" />
-    </div><!-- .hangman-image -->
+    </div><!-- .hangman-area -->
 
-    <div class="well span9 guess-area">
-
-      <form action="routes.php" method="post" id="guess-form">
-        <input name="letter" type="text" placeholder="Letter" />
-        <br>
-        <button type="submit" class="btn">Guess!</button>
-      </form>
-      
+    <div class="span5 guess-area">
+      <div class="row">
+        <div class="well span5 previous-guesses">
+        </div>
+      </div>
+      <div class="row">
+        <div class="well span5 new-guesses">
+          <form action="routes.php" method="post" id="guess-form">
+            <input name="letter" type="text" placeholder="Letter" />
+            <br>
+            <button type="submit" class="btn">Guess!</button>
+          </form>
+        </div>
+      </div>
     </div><!-- .guess-area -->
-
-  </div>
+  </div><!-- #hangman -->
 
 <?php include('templates/footer.php') ?>
